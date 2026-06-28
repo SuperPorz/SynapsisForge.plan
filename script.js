8a5da1fec3eb690cf78152cda60418f1cb4a20c8
michelangelo stega <mikybeeh@hotmail.it>
2026-06-28 22:53:43 +0200

    updated new actual phase & updated project's progress

--- script.js
+++ script.js
@@ -380,5 +380,5 @@ function toggle(id) {
 document.addEventListener('DOMContentLoaded', () => {
   inizializzaApplicazione();
   // Keep a chosen phase open — CAMBIARE FASE QUI
-  document.getElementById('ph7')?.classList.add('open');
+  document.getElementById('ph8')?.classList.add('open');
 });
